108442ff60f49a567e469c59b023341c069bbcfc
Markus Tyrkkö <markus.tyrkko@nitor.com>
2025-04-12 21:58:29 +0300

    Updates

---
+++ _next/static/chunks/app/layout-99d3c7ff43337aae.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{77047:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,94583,23))},94583:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[838,441,684,358],()=>s(77047)),_N_E=e.O()}]);
